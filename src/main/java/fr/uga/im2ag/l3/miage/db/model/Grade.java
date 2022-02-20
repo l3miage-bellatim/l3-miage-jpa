58325ffd17130092016bd713f4aff1d8e819639d
MamouneBellatif <bellatif@yahoo.fr>
2022-02-20 19:43:56 +0100

    grade + test (incomplet)

--- src/main/java/fr/uga/im2ag/l3/miage/db/model/Grade.java
+++ src/main/java/fr/uga/im2ag/l3/miage/db/model/Grade.java
@@ -1,11 +1,22 @@
 package fr.uga.im2ag.l3.miage.db.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
 
 // TODO ajouter une named query pour une des requêtes à faire dans le repository
+@Entity
+@NamedQuery(name="Marin.findAll", query="select grade from Grade grade")
 public class Grade {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @OneToOne
     private Subject subject;
     @Column(name = "grade")
     private Float value;
--- src/test/java/fr/uga/im2ag/l3/miage/db/repository/GradeTest.java
+++ src/test/java/fr/uga/im2ag/l3/miage/db/repository/GradeTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class GradeTest extends Base {
 
     GradeRepository gradeRepository;
@@ -23,7 +25,18 @@ class GradeTest extends Base {
 
     @Test
     void shouldSaveGrade() {
-        // TODO
+        //TODO tester les autres champs ? tester la cle etrangere
+        final var grade = Fixtures.createGrade(Fixtures.createSubject()); //on creer un nouvet sujet a ajouter a la note
+        entityManager.getTransaction().begin();
+        entityManager.persist(grade.getSubject());  //on rend le sujet de la note persistant, sinon le test lève une exception au commit
+        gradeRepository.save(grade);
+        entityManager.getTransaction().commit();
+        entityManager.detach(grade);
+
+        var pGrade = gradeRepository.findById(grade.getId());
+        assertThat(pGrade).isNotNull().isNotSameAs(grade);
+        assertThat(pGrade.getValue()).isEqualTo(grade.getValue());
+
     }
 
     @Test
